276a1765697c51f372dd18e22edaaf72ec0972a8
Andremorgh <a.morghen@studenti.unina.it>
2024-06-04 16:32:22 +0200

    hw4_04/06

--- HW4/quadruped_simulation/fcns/get_params.m
+++ HW4/quadruped_simulation/fcns/get_params.m
@@ -62,7 +62,7 @@ end
 p.mass = 5.5;
 p.J = diag([0.026,0.112,0.075]);
 p.g = 9.81;
-p.mu = 1;       % friction coefficient
+p.mu = 0.5;       % friction coefficient
 p.z0 = 0.2;     % nominal COM height
 p.pf34 = [[0.15;0.094;0],[0.15;-0.094;0],[-0.15;0.094;0],[-0.15;-0.094;0]];
 
